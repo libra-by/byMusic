14a9a20a6123a04687e122348c4ef1855b67a35c
libra-by <17640359719@163.com>
2020-02-25 20:20:17 +0800

    demoMusic

---
+++ electron-vue-cloud-music-master/src/main/windows/updateWindow.js
@@ -0,0 +1,42 @@
+import { LOAD_URL } from './../config'
+const miniWinURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080/#update`
+  : `${LOAD_URL}#update`
+const previewIcon = process.env.NODE_ENV === 'development' ? '/images/tray.ico' : `${global.__static}/images/tray.ico`
+let updateWindow
+const createUpdateWindow = function (BrowserWindow) {
+  if (updateWindow) return false
+  let obj = {
+    icon: previewIcon,
+    height: 420,
+    minHeight: 420,
+    width: 340,
+    minWidth: 340,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: process.env.NODE_ENV === 'development',
+    transparent: process.platform !== 'linux',
+    alwaysOnTop: true,
+    // parent: global.mainWindow,
+    // modal: true,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      webSecurity: false,
+      navigateOnDragDrop: true
+      // devTools: false
+    }
+  }
+  updateWindow = new BrowserWindow(obj)
+  updateWindow.loadURL(miniWinURL)
+
+  updateWindow.on('ready-to-show', () => {
+    updateWindow.show()
+  })
+  updateWindow.on('closed', () => {
+    updateWindow = null
+  })
+  return updateWindow
+}
+export default createUpdateWindow
